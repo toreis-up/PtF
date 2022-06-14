375a404e1b91b7b64ce05dfe9db807b4ae0d3921
tos-up <j20440@inc.kisarazu.ac.jp>
2022-06-14 14:18:34 +0900

    initしまくってます。

---
+++ components/problemComponents/ListProblems.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <v-container>
+      <v-btn @click="getProblems">取得</v-btn>
+      <v-data-table :headers="listHeader" :items="problems" no-data-text="データがありません。">
+        <template v-slot:item.name="props">
+          <v-btn :href=props.item.url target="_blank" text>{{props.item.name}}</v-btn>
+        </template>
+      </v-data-table>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import {getFirestore, collection, doc, getDoc, getDocs, query} from 'firebase/firestore';
+export default {
+data() {
+  return {
+    listHeader: [
+      {text: "pdf名", value: "name"},
+      {text: "担当教員", value: "teacher"},
+      {text: "学年", value: "grade"},
+      {text: "年度", value: "year"}
+    ],
+    problems: []
+  }
+},
+methods: {
+  async getProblems () {
+    const db = await getFirestore();
+    const probColRef = collection(db, "problems");
+    const querySnapshot = await getDocs(probColRef);
+    querySnapshot.forEach((doc) => {
+      if (this.problems.includes(doc.data()) === true);
+      else this.problems.push(doc.data());
+    })
+  }
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <Tutorial />
+  <ListProblems />
+</div>
+</template>
+
+<script>
+import ListProblems from '../components/problemComponents/ListProblems.vue'
+export default {
+  name: 'IndexPage',
+  components: { ListProblems }
+}
+</script>
