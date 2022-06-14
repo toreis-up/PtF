a111787f32932eff91e470f0b6f9389b24f800f4
tos-up <j20440@inc.kisarazu.ac.jp>
2022-06-14 21:15:15 +0900

    眠くなった

---
+++ components/problemComponents/SubmitProblem.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field label="教科名"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
