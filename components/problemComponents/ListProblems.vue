c73f2dd0cf47f415dc5769a073317f8588a0d025
tos-up <j20440@inc.kisarazu.ac.jp>
2022-06-14 14:09:08 +0900

    ちょっとだけ部品作った

---
+++ components/problemComponents/ListProblems.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <v-container>
+      <v-btn @click="getProglems">取得</v-btn>
+      <v-data-table :headers="listHeader" :items="problems">
+
+      </v-data-table>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import {collection, doc, getDoc, getDocs} from 'firebase/firestore';
+export default {
+data() {
+  return {
+    listHeader: [],
+    problems: []
+  }
+},
+methods: {
+  async getProblems () {
+    const q = query(collection(db, "problems"))
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data()); 
+    })
+  }
+},
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from "vuetify/es5/util/colors";
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -19,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    './plugins/firebase.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -26,17 +29,33 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    '@nuxtjs/vuetify',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa'
   ],
+
+// Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+vuetify: {
+  customVariables: ["~/assets/variables.scss"],
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3,
+      },
+    },
+  },
+},
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
