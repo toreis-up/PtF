b1dcd0865684179d156752767c770ad070db8541
Haruto_h13 <j20423@kisarazu.kosen-ac.jp>
2022-06-15 22:18:10 +0900

    v-radioで学年選択
    学年をtext-fieldからradioにして選択できるようにした(多分あってる気がする)

--- components/problemComponents/SubmitProblem.vue
+++ components/problemComponents/SubmitProblem.vue
@@ -23,7 +23,9 @@
             <v-card>
               <v-card-subtitle> 学年 </v-card-subtitle>
               <v-card-text>
-               <v-text-field v-model="grade"></v-text-field>
+               <v-radio-group row v-model="grade_school">
+                <v-radio v-for="gra in grade" :key="gra.name" :label="gra.name" :value="gra.val"></v-radio>
+               </v-radio-group>
               </v-card-text>
             </v-card>
           </v-col>
@@ -31,7 +33,7 @@
             <v-card>
               <v-card-subtitle> 中間 or 期末 </v-card-subtitle>
               <v-card-text>
-                <v-radio-group v-model="CorK">
+                <v-radio-group row v-model="CorK">
                   <v-radio v-for="btn in buttons" :key="btn.name" :label="btn.name" :value="btn.val"></v-radio>
                 </v-radio-group>
               </v-card-text>
@@ -57,7 +59,14 @@ export default {
       return {
       subject_name: "",
       year: "",
-      grade: "",
+      grade_school: 1,
+      grade: [
+        {name: "1年", val: "1"},
+        {name: "2年", val: "2"},
+        {name: "3年", val: "3"},
+        {name: "4年", val: "4"},
+        {name: "5年", val: "5"}
+      ],
       staff_name: "",
       CorK: 1,
       buttons:[
