6bb2d1e9b45b983090c6f270a62db7b6d271cb76
tos-up <j20440@inc.kisarazu.ac.jp>
2022-06-16 22:02:21 +0900

    いらんもんポーイ！！！！！

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,15 @@
 <template>
-<div>
-  <Tutorial />
-  <ListProblems />
-  <SubmitProblem/>
-</div>
+  <div>
+    <ListProblems />
+    <SubmitProblem />
+  </div>
 </template>
 
 <script>
-import SubmitProblem from '../components/problemComponents/SubmitProblem.vue'
-import ListProblems from '../components/problemComponents/ListProblems.vue'
+import SubmitProblem from "../components/problemComponents/SubmitProblem.vue";
+import ListProblems from "../components/problemComponents/ListProblems.vue";
 export default {
-  name: 'IndexPage',
-  components: { ListProblems, SubmitProblem }
-}
+  name: "IndexPage",
+  components: { ListProblems, SubmitProblem },
+};
 </script>
