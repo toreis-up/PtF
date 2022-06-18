a9e876ec2241c355d0500235965bffd460f64fec
tosup_msi <j20440@inc.kisarazu.ac.jp>
2022-06-18 15:29:01 +0900

    直してみたよ

--- pages/index.vue
+++ pages/index.vue
@@ -14,8 +14,23 @@ export default {
 
   middleware: "auth",
   data() {
-    return{
-      User:this.$store.state.authUser
+    return {
+      User: this.$store.state.authUser,
+    };
+  },
+  mounted() {
+    if (
+      localStorage.getItem("authDisplayName") &&
+      localStorage.getItem("authUID")
+    ) {
+      this.$store.commit("setUser", {
+        user: {
+          displayName: localStorage.getItem("authDisplayName"),
+          uid: localStorage.getItem("authUID"),
+        },
+      });
+    } else {
+      this.$router.push("/login");
     }
   },
 };
