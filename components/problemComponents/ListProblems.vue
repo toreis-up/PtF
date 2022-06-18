236f6a5ef6cb9cfe5a332b03d70b911bd28e275f
tosup_msi <j20440@inc.kisarazu.ac.jp>
2022-06-18 22:47:36 +0900

    教示用仮修正

--- components/problemComponents/ListProblems.vue
+++ components/problemComponents/ListProblems.vue
@@ -2,21 +2,17 @@
   <section>
     <v-container>
       <v-btn @click="getProblems">取得</v-btn>
-      <v-card>
-        <v-card-title>
-          検索欄
-          <v-spacer></v-spacer>
-          <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details>
-          </v-text-field>
-        </v-card-title>
-        <v-data-table :headers="listHeader" :items="problems" no-data-text="データがありません。">
-          <template v-slot:[`item.name`]="props">
-            <v-btn :href="props.item.url" target="_blank" text>{{
-              props.item.name
-              }}</v-btn>
-          </template>
-        </v-data-table>
-      </v-card>
+      <v-data-table
+        :headers="listHeader"
+        :items="problems"
+        no-data-text="データがありません。"
+      >
+        <template v-slot:[`item.name`]="props">
+          <v-btn :href="props.item.url" target="_blank" text>{{
+            props.item.name
+          }}</v-btn>
+        </template>
+      </v-data-table>
     </v-container>
   </section>
 </template>
@@ -31,31 +27,33 @@ import {
   query,
 } from "firebase/firestore";
 export default {
-    data() {
-        return {
-            search: "",
-            listHeader: [
-                { text: "pdf名", value: "name" },
-                { text: "担当教員", value: "teacher" },
-                { text: "学年", value: "grade" },
-                { text: "年度", value: "year" },
-            ],
-            problems: [],
-        };
+  data() {
+    return {
+      listHeader: [
+        { text: "pdf名", value: "name" },
+        { text: "教科名", value: "Subject" },
+        { text: "担当教員", value: "Staff_name" },
+        { text: "学年", value: "Grade" },
+        { text: "年度", value: "Year" },
+        { text: "開催期", value: "C_or_K" },
+      ],
+      problems: [],
+    };
+  },
+  methods: {
+    async getProblems() {
+      const db = await getFirestore();
+      const probColRef = collection(db, "problems");
+      const querySnapshot = await getDocs(probColRef);
+      querySnapshot.forEach((doc) => {
+        if (this.problems.includes(doc.data()) === true);
+        else this.problems.push(doc.data());
+      });
     },
-    methods: {
-        async getProblems() {
-            const db = await getFirestore();
-            const probColRef = collection(db, "problems");
-            const querySnapshot = await getDocs(probColRef);
-            querySnapshot.forEach((doc) => {
-                if (this.problems.includes(doc.data()) === true)
-                    ;
-                else
-                    this.problems.push(doc.data());
-            });
-        },
-    },
+  },
+  mounted() {
+    this.getProblems();
+  },
 };
 </script>
 
